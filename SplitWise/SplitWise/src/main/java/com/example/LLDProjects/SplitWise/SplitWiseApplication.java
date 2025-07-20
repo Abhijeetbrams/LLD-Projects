846ee63220e2488506b6933ae2cf4c735d2914c8
Abhijeet Brahmchari <abybrams@gmail.com>
2025-07-20 21:16:06 +0530

    Some Rndom changes

--- SplitWise/SplitWise/src/main/java/com/example/LLDProjects/SplitWise/SplitWiseApplication.java
+++ SplitWise/SplitWise/src/main/java/com/example/LLDProjects/SplitWise/SplitWiseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SplitWiseApplication {
 	// Application entry point for the SplitWise application
 	// This class is responsible for bootstrapping the Spring Boot application
+    // Changes made to the original code:
 
 	public static void main(String[] args) {
 		SpringApplication.run(SplitWiseApplication.class, args);
