f9b63e0af2be1ff46a66c798b62c8a229502ac75
Abhijeet Brahmchari <abybrams@gmail.com>
2025-07-23 14:34:12 +0530

    Changes done

--- SplitWise/SplitWise/src/main/java/com/example/LLDProjects/SplitWise/SplitWiseApplication.java
+++ SplitWise/SplitWise/src/main/java/com/example/LLDProjects/SplitWise/SplitWiseApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SplitWiseApplication {
 	// Application entry point for the SplitWise application
-	// This class is responsible for bootstrapping the Spring Boot application
-    // Changes made to the original code:
-	// changes
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SplitWiseApplication.class, args);
